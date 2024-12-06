3396db7d60c302564a6fd9149c1296678d891a7e
Kristina047 <ch.drobot@gmail.com>
2024-12-06 16:56:24 +0300

    дз массивы 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задача 1");
+        int [] arr = {1,2,3,4,5};
+        int sum = 0;
+        for (int element : arr) {
+            sum = sum + element;
+        }
+        System.out.println("Сумма трат за месяц составила "+sum+" рублей.");
+
+        System.out.println("Задача 2");
+
     }
 }
