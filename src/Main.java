4eb0d741ab0505bd8b4a6d7992a7ecd8784b8667
Kristina047 <ch.drobot@gmail.com>
2024-12-07 15:53:48 +0300

    дз массивы 2

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,31 @@ public class Main {
 
         System.out.println("Задача 2");
         int[] arr1 = {1, 2, 3, 4, 5};
-        int maxArr1 = 0;
+        int maxArr1 = arr1[0];
+        int minArr1 = arr1[0];
         for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] > maxArr1) {
                 maxArr1 = arr1[i];
             }
+            if (arr1[i] < minArr1) {
+                minArr1 = arr1[i];
+            }
         }
-        System.out.println(maxArr1);
+        System.out.println("Минимальная сумма трат за неделю составила " + minArr1 + " рубль. Максимальная сумма трат за неделю составил " + maxArr1 + " рублей");
+
+        System.out.println("Задача 3");
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            sum1+= arr2 [i];
+            sum2 = sum1/arr2.length;
+        }
+        System.out.println("Средняя сумма трат за месяц составила "+sum2+" рубля");
+
+
+
+
     }
 }
 
